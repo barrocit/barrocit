9b89dbcd40b32c1626f771a0e4a4e11fa01b0b4e
nick076 <nickdekoning@ziggo.nl>
2014-10-02 11:57:43 +0200

    width

--- app/login.php
+++ app/login.php
@@ -3,34 +3,29 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>PHP Series | Login en sessies</title>
+	<title>Barroc-IT | Inloggen</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 		<div class="top">Barroc-IT</div>
 			<div class="login">
-				<form method="post" action="controllers/authController.php" role="form" class="login-form col-md-4 col-md-offset-4">
+				<form method="post" action="controllers/authController.php" role="form">
 					<h2>Inloggen</h2>
-					<ul>
-						<?php  
-						// succes of fail message
-						if (isset($_GET['msg'])) {
-						echo '<li>' .  htmlspecialchars($_GET['msg']) . '</li>';
-						}
-						?>
-					</ul>
-
-					<div class="form-group">
-						<div class="login-width"><label for="email">Email</label></div>
+						<ul>
+							<?php  
+							// succes of fail message
+							if (isset($_GET['msg'])) {
+							echo '<li>' .  htmlspecialchars($_GET['msg']) . '</li>';
+							}
+							?>
+						</ul>
+						<div class="width-tabel"><label for="email">Email</label></div>
 						<input type="email" name="email" id="email" class="form-control">
-					</div>
-					<div class="form-group">
-						<div class="login-width"><label for="password">Password</label></div>
+				
+						<div class="width-tabel"><label for="password">Password</label></div>
 						<input type="password" name="password" id="password" class="form-control">
-					</div>
-					<div class="form-group">
+
 						<input type="submit" name="authUser" value="Login" class="btn btn-large">
-					</div>
 				</form>
 			</div>
 		</div>	

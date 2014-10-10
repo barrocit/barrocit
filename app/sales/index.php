1bf2350b77eeb30a1b946f81f669832759fe7230
Jessepeffer <jesse1995peffer@hotmail.com>
2014-10-10 14:50:22 +0200

    sales
    sales

---
+++ app/sales/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../bootstrap/bootstrapStyle.css">
+	<title>Sales</title>
+    <header>
+		<h1>Sales</h1>
+	</header>
+</head>
+
+<body>
+
+<div class="navbar navbar-default">
+  <div class="navbar-header">
+    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="#"></a>
+  </div>
+</div>
+  <table class="overzicht_custommers">
+                    <thead>
+                        <tr>
+                            <td class="col-sm-2"><strong>CompanyName</strong></td>
+                            <td class="col-sm-3"><strong>Adress1</strong></td>        
+                            <td class="col-sm-1"><strong>Zipcode1</strong></td>
+                            <td class="col-sm-2"><strong>Residence1</strong></td>
+                            <td class="col-sm-2"><strong>Email</strong></td>        
+                            <td class="col-sm-1"><strong>Prospect</strong></td>
+                            <td class="col-sm-1"><strong>CreditWorthy</strong></td>
+                            <td class="col-sm-1"><strong>Action</strong></td>
+                        </tr>
+                    </thead>
+</body>
+<footer>
+
+</footer>
+</body>
+</html>

8e069f789cb557941b18556a6e63e89586d1bd89
frisokin <d194188@edu.rocwb.nl>
2014-10-10 12:23:18 +0200

    developement update

--- app/development/index.php
+++ app/development/index.php
@@ -1,1 +1,26 @@
-development
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../bootstrap/bootstrapStyle.css">
+	<title>development</title>
+</head>
+
+<body>
+	<header>
+		<h1>Development</h1>
+	</header>
+<div class="navbar navbar-default">
+  <div class="navbar-header">
+    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="#">Deactivated projects</a>
+  </div>
+</div>
+
+
+<footer>
+
+</footer>
+</body>
+</html>

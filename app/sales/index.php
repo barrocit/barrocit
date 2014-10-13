b8eb27d8cb1970a929e338923dbcd89996cbb4e8
Jessepeffer <jesse1995peffer@hotmail.com>
2014-10-13 14:02:17 +0200

    sales update

--- app/sales/index.php
+++ app/sales/index.php
@@ -20,15 +20,36 @@
   <table class="overzicht_custommers">
                     <thead>
                         <tr>
-                            <td class="col-sm-2"><strong>CompanyName</strong></td>
-                            <td class="col-sm-3"><strong>Adress1</strong></td>        
-                            <td class="col-sm-1"><strong>Zipcode1</strong></td>
-                            <td class="col-sm-2"><strong>Residence1</strong></td>
-                            <td class="col-sm-2"><strong>Email</strong></td>        
-                            <td class="col-sm-1"><strong>Prospect</strong></td>
-                            <td class="col-sm-1"><strong>CreditWorthy</strong></td>
-                            <td class="col-sm-1"><strong>Action</strong></td>
+                            <td class="col-sm-2"><strong>companyName</strong></td>
+                            <td class="col-sm-3"><strong>adress1</strong></td>        
+                            <td class="col-sm-1"><strong>zipcode1</strong></td>
+                            <td class="col-sm-2"><strong>residence1</strong></td>
+                            <td class="col-sm-2"><strong>email</strong></td>        
+                            <td class="col-sm-1"><strong>prospect</strong></td>
+                            <td class="col-sm-1"><strong>creditWorthy</strong></td>
+                            <td class="col-sm-1"><strong>action</strong></td>
                         </tr>
+                                     <?php
+                    while ($row = mysqli_fetch_assoc($query)) 
+                    {
+                        $id=$row['CustomerNR'];
+                        echo '<tr>';
+                        echo '<td>' . $row['companyName'] . '</td>';
+                        echo '<td>' . $row['adress1'] . '</td>';
+                        echo '<td>' . $row['zipcode1'] . '</td>';
+                        echo '<td>' . $row['residence1'] . '</td>';
+                        echo '<td>' . $row['email'] . '</td>';
+                        echo '<td>' . $row['prospect'] . '</td>';
+                        echo '<td>' . $row['creditWorthy'] . '</td>';
+                        ?>
+                        <html>
+                        <td>  <a href="view.php?id=<?php echo $id;?>" class="btn btn-info">View</a>
+                        <td> <a href="edit.php?id=<?php echo $id;?>" class="btn btn-primary">Edit</a>
+                        </html>
+                        <?php
+                        echo '</tr>';
+                    }
+                    ?>
                     </thead>
 </div>
 </body>

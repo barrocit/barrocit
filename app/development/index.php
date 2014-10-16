7a14f9c4289c588afdf2d855277a658210fbfdc5
frisokin <d194188@edu.rocwb.nl>
2014-10-16 09:02:56 +0200

    Update development

--- app/development/index.php
+++ app/development/index.php
@@ -11,13 +11,17 @@
 
 <body>
 	<header>
-		<h1>Development</h1>
+    
 	</header>
 
 <div class="container">
+
+<img src="../images/jumbotrondev.jpg" width="1140" height= "400px;"/>
+
 <div class="navbar navbar-default">
   <div class="navbar-header">
     <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="#">Project</a>
     <a class="navbar-brand" href="#">Deactivated projects</a>
   </div>
 </div>
@@ -27,11 +31,13 @@
       <tr>
         <th>Company name</th>
         <th>Contact person</th>
+        <th>Address</th>
+        <th>Zip code</th>
+        <th>Residence</th>
+        <th>Telephone number</th>
+        <th>Fax number</th>
+        <th>Email</th>
         <th>Open projects</th>
-        <th>Last contact date</th>
-        <th>Balance</th>
-        <th>Limit</th>
-        <th>Edit</th>
         <th>Deactivate</th>
       </tr>
     </thead>
@@ -45,11 +51,13 @@
           while ($row = mysqli_fetch_assoc($query)){
             echo '<tr><td>' . $row['companyName'] . '</td>';
             echo '<td>' . $row['contactPerson'] . '</td>';
+            echo '<td>' . $row['address'] . '</td>';
+            echo '<td>' . $row['zipCode'] . '</td>';
+            echo '<td>' . $row['residence'] . '</td>';
+            echo '<td>' . $row['telephoneNumber'] . '</td>';
+            echo '<td>' . $row['faxNumber'] . '</td>';
+            echo '<td>' . $row['email'] . '</td>';
             echo '<td>' . $row['openProjects'] . '</td>';
-            echo '<td>' . $row['lastcontactDate'] . '</td>';
-            echo '<td>' . $row['balance'] . '</td>';
-            echo '<td>' . $row['limit'] . '</td>';
-            echo '<td> <a href="edit.php?id=' . $row['customerNR'] . '"><img src="bewerken.png"></a>  </td>';
             echo '<td> <a href="deactivateProject.php?id=' . $row['customerNR'] . '"><img src="verwijderen.png"></a> </td></tr>';
           }
         ?>   

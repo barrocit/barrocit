cbfc5eb6776d7574eff01b8e98b303ff8c71f7f3
nick076 <nickdekoning@ziggo.nl>
2014-11-07 14:59:04 +0100

    updates

--- app/finance/index.php
+++ app/finance/index.php
@@ -24,7 +24,7 @@
   </div>
   <div class="navbar-collapse collapse navbar-responsive-collapse">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Index</a></li>
+      <li class="active"><a href="index.php">Index</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="http://localhost/GitHub/barrocit/app/development/logout.php">Log-out</a></li>
